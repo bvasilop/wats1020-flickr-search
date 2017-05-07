63e59115f335b927d87450d652472350329f6d8f
bvasilop <bvasilop@gmail.com>
2017-05-06 17:29:23 -0700

    Added mouseover color change to buttons

--- js/main.js
+++ js/main.js
@@ -1,55 +1,10 @@
-// Asynchronous Flickr Search
-//
-// Flickr reveals a searchable JSON Feed you can access via jQuery's $.getJSON()
-// method. Use this to allow users to search for a tag or comma-separated list
-// of tags and view the images that are found.
-//
-// Allow users to click the images to see a larger version with more information.
-
-    // Place your code here, inside the document ready handler.
-
-    // Create a function called `searchImages()`. This function will handle the
-    // process of taking a user's search terms and sending them to Flickr for a
-    // response.
-
-    // Inside the `searchImages()` function, the following things should happen:
-
-        // 1.   Accept a string value called `tags` as an argument. Example:
-        //      `var searchPhotos = function(tags){`
-        //
-        // 2.   Define the location of the Flickr API like this:
-        //      `var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";`
-        //
-        // 3.   Construct a `$.getJSON()` call where you send a request object
-        //      including the tags the user submitted, and a `done()` handler
-        //      that displays and refreshes the content appropriately.
-        //
-        // 4.   Update the display to add the images to the list with the id
-        //      `#images`.
-
-    // Attach an event to the search button (`button.search`) to execute the
-    // search when clicked.
-
-        // When the Search button is clicked, the following should happen:
-        //
-        // 1.   Prevent the default event execution so the browser doesn't
-        //      Example: `event.preventDefault();`
-        //
-        // 2.   Get the value of the 'input[name="searchText"]' and use that
-        //      as the `tags` value you send to `searchImages()`.
-        //
-        // 3.   Execute the `searchImages()` function to fetch images for the
-        //      user.
-
-    // STRETCH GOAL: Add a "more info" popup using the technique shown on the
-    // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
-
 
         $(document).on('ready', function(){
             //hide add images button on page load with ID hide
             $('#hide').hide();
             // 1.   Accept a string value called `tags` as an argument.
             var searchImages = function(tags) {
+           // 2. Define the location of the Flickr API
                 var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
                 console.log(tags);
                 $('#images').innerHTML = '<li class="search-throbber">Searching...</li>';
@@ -71,12 +26,7 @@
                         $( "<img>").attr("src", item.media.m).appendTo(newListItem);
                         $("<br><br>").appendTo(newListItem);
 
-                        /*var newTitle = $('<p class="image-title">').html(item.title).appendTo(newListItem);
-                        var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newListItem);
-                        var newDescription = $('<p class="image-description">').html(item.description).appendTo(newListItem);
-                        var newLink = $('<a>').attr('href', item.link).text('View on Flickr.').appendTo(newListItem);
-                        */
-                        var newButton = $("<button class='btn btn-sm btn-primary'>enlarge</button>").attr({
+                  var newButton = $("<button class='btn btn-sm btn-primary'>enlarge</button>").attr({
                             'data-title': item.title,
                             'data-toggle': "modal",
                             'data-target': "#infoModal",
@@ -89,24 +39,19 @@
 
                         //add CSS to page when search results appear
                         $("#images").css("column-count", "4");
-                        $("ul#images li").css("background-color", "#DDDDDD");
+                        $("ul#images li").css("background-color", "#00245D");
                         $("ul#images li").css("box-shadow", "2px 2px 4px 0 #ccc");
                         //number of items to search for
                         if ( i === 15 ) {
                             return false;
                         }
-
-                        //hide li elements after search
+                       //hide li elements after search
                         $(function(){
                             $('li').hide()
                             })
                         });
-
-                });
+                    });
             };
-
-
-
             $("button.search").on("click", function(event) {
                 //show add images button with ID hidden
                 $('#hide').show();
@@ -120,9 +65,9 @@
                 //      user.
                 console.log(searchTextInput);
                 searchImages(searchTextInput.value);
-
             });
 
+                // Modal popup section
             $('#infoModal').on('show.bs.modal', function (event) {
                 var button = $(event.relatedTarget); // Button that triggered the modal
                 var title = button.data('title'); // Extract info from data-* attributes
@@ -136,12 +81,10 @@
                 modalBody.empty();
                 var modalDescription = $("<p class='image-description'>").html(imageDescription).appendTo(modalBody);
             });
+                //add new image function
+                $('.container').each(function() {
 
-
-            //add new image function
-            $('.container').each(function() {
-
-                //find first().siblings() and hide()
+            //find first().siblings() and hide()
                 $(this).find('li').first().siblings().hide();
 
                 //when add button is pressed find li:first child of container
